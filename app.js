0f030f4b0d7cd64d9946fa6c0c189bbb4c4a726c
pizarrosch <bayenrshc89@gmail.com>
2023-10-07 01:21:05 +0200

    Handle request body and use body-parser to parse the data

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-const http = require('http');
+// const http = require('http'); //Needed only if app.listen is not used
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  console.log('In product middleware');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res) => {
+  console.log(req.body)
+  res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from express</h1>');
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+// const server = http.createServer(app);
+// server.listen(3000);
+
+//The shorter version of two lines above
+app.listen(3000);
