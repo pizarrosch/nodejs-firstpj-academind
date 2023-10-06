4c6769ae25c9a5fd0de221f195c4316ca53e2ac6
pizarrosch <bayenrshc89@gmail.com>
2023-10-07 01:47:38 +0200

    Add 'admin' path for function from admin.js file

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ const shopRouter = require('./routes/shop');
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
 
 // const server = http.createServer(app);
 // server.listen(3000);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
 //The shorter version of two lines above
 app.listen(3000);
