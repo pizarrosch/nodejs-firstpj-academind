3749c8ba305d6dd76d117ca78029d24ebc38acb4
pizarrosch <bayenrshc89@gmail.com>
2023-10-07 16:16:40 +0200

    Create static path to serve external css files from public folder

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
+const {static} = require("express");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
